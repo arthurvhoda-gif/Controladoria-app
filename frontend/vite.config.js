c62dc4d7f15b244dfa8e89720b76da3299e5c3b8
Seu Nomearthurvhoda-gif <arthur.v.hoda@gmail.com>
2026-09-01 11:07:57 -0300

    Transformando em App Oficial com VitePWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,32 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Controladoria App',
+        short_name: 'Controladoria',
+        description: 'Sistema de Gestão da Controladoria',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3256/3256013.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://cdn-icons-png.flaticon.com/512/3256/3256013.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
