629ef35a8924c6d8fa48714058bf800887f399bb
Seu Nomearthurvhoda-gif <arthur.v.hoda@gmail.com>
2026-09-01 10:49:37 -0300

    Adicionando Vercel na lista VIP do CORS

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,11 @@ app = FastAPI(title="Controladoria", version="1.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173",
+        "https://controladoria-app-lilac.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
