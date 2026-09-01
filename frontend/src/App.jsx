c0d4724b2fc9b032cd15dbaf470f0613525f1a2e
Seu Nomearthurvhoda-gif <arthur.v.hoda@gmail.com>
2026-09-01 09:45:00 -0300

    Atualizando link da API para o Render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import {
 } from 'recharts'
 import './App.css'
 
-const API_URL = 'http://127.0.0.1:8000'
+const API_URL = 'https://controladoria-api.onrender.com'
 
 function App() {
   const [usuarioLogado, setUsuarioLogado] = useState(() => {
